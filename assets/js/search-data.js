22c4109b77f33a8e5d4cc15b6ca7f713ec7f8083
Jvient <46488188+Jvient@users.noreply.github.com>
2026-04-27 07:33:02 +0000

    Deploying to gh-pages from @ Jvient/Jvient.github.io@4acac83bb71d3925ac9dee64875bbea4bf0a3682 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -410,16 +410,6 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
             },},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
